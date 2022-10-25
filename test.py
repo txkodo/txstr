156d348296eec401b7ed034c23b8a839d7b82a46
txkodo <kodo.hsmkai@gmail.com>
2022-10-26 02:00:30 +0900

    deleted python file

--- test.py
+++
@@ -1,5 +0,0 @@
-
-a = """{"extra":[{"color":"red","extra":[{"translate":"commands.enchant.failed.entity","with":["[18:15:10] Marker has 3 tags: a, b, c, {\\"extra\\":[{\\"color\\":\\"red\\",\\"extra\\":[{\\"translate\\":\\"commands.enchant.failed.entity\\",\\"with\\":[\\"abcde￿\\"]}],\\"text\\":\\"\\"}],\\"text\\":\\"[18:15:10] \\"}, {\\"extra\\":[{\\"color\\":\\"red\\",\\"extra\\":[{\\"translate\\":\\"commands.enchant.failed.entity\\",\\"with\\":[\\"xyz￿\\"]}],\\"text\\":\\"\\"}],\\"text\\":\\"[18:15:10] \\"}"]}],"text":""}],"text":"[18:15:10] "}"""
-b = """{"extra":[{"color":"red","extra":[{"translate":"commands.enchant.failed.entity","with":["[18:15:10] Marker has 3 tags: a, b, c, {\\"extra\\":[{\\"color\\":\\"red\\",\\"extra\\":[{\\"translate\\":\\"commands.enchant.failed.entity\\",\\"with\\":[\\"abcde￿\\"]}],\\"text\\":\\"\\"}],\\"text\\":\\"[18:15:10] \\"}, {\\"extra\\":[{\\"color\\":\\"red\\",\\"extra\\":[{\\"translate\\":\\"commands.enchant.failed.entity\\",\\"with\\":[\\"xyz￿\\"]}],\\"text\\":\\"\\"}],\\"text\\":\\"[18:15:10] \\"}"]}],"text":""}],"text":"[18:15:10] "}"""
-
-print(a == b)
